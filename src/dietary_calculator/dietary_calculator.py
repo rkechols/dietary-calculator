cc56d32aca1479918f43f151959f7654e11dbf93
Ryan Echols <rkechols21@gmail.com>
2022-07-27 02:42:29 -0600

    very basic calculation

--- src/dietary_calculator/dietary_calculator.py
+++ src/dietary_calculator/dietary_calculator.py
@@ -1,2 +1,15 @@
+from typing import Set
+
+from dietary_calculator.attribute import DietaryAttributeHierarchy
+from dietary_calculator.product import FoodProduct
+
+
 class DietaryCalculator:
-    pass
+    def __init__(self, attribute_hierarchy: DietaryAttributeHierarchy = None):
+        self.attribute_hierarchy = attribute_hierarchy if attribute_hierarchy is not None else DietaryAttributeHierarchy()
+
+    def get_all_classifications(self, food: FoodProduct) -> Set[str]:
+        to_return = set()
+        for base_attribute in food.attributes:
+            to_return.update(self.attribute_hierarchy.get_all_classifications(base_attribute))
+        return to_return
