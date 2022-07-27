928bd19e0100d64769c5e310a5315ff1ce07fd7c
Ryan Echols <rkechols21@gmail.com>
2022-07-27 01:59:56 -0600

    basic attribute hierarchy

--- src/dietary_calculator/attribute.py
+++ src/dietary_calculator/attribute.py
@@ -1,2 +1,54 @@
+from typing import Dict, Set
+
+
 class DietaryAttribute:
-    pass
+    """
+    Really just a hierarchy of strings
+    """
+    DAIRY = "dairy"
+    EGG = "egg"
+    MEAT = "meat"
+    FISH = "fish"
+    SHELLFISH = "shellfish"
+    PORK = "pork"
+    ANIMAL_PRODUCT = "animal product"
+    WHEAT = "wheat"
+    GLUTEN = "gluten"
+    NUTS = "nuts"
+    SOY = "soy"
+    ALCOHOL = "alcohol"
+    COFFEE = "coffee"
+    TEA = "tea"
+
+    # key is an attribute,
+    # value is a set of attributes that are "parents" of the key
+    hierarchy: Dict[str, Set[str]] = {
+        DAIRY: {ANIMAL_PRODUCT},
+        EGG: {ANIMAL_PRODUCT},
+        MEAT: {ANIMAL_PRODUCT},
+        FISH: {MEAT},
+        SHELLFISH: {FISH},
+        PORK: {MEAT},
+        WHEAT: {GLUTEN}
+    }
+
+    @classmethod
+    def get_all_classifications(cls, attribute: str) -> Set[str]:
+        """
+        Get all the ways of naming an attribute,
+        i.e. itself and all of its parents / ancestors in the hierarchy.
+        """
+        all_seen: Set[str] = set()
+        this_level: Set[str] = {attribute}
+        while len(this_level) > 0:
+            # record this level
+            all_seen.update(this_level)
+            # get all the parents
+            next_level: Set[str] = set()
+            for attribute in this_level:
+                next_level.update(cls.hierarchy.get(attribute, set()))
+            # discard any parents that we've already seen
+            next_level -= all_seen
+            # move to the next level
+            this_level = next_level
+        return all_seen
