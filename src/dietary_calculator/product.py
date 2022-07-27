48ab0a3cd1668c53af5609d990a95527d43b4af0
Ryan Echols <rkechols21@gmail.com>
2022-07-27 02:35:52 -0600

    set up FoodProduct

--- src/dietary_calculator/product.py
+++ src/dietary_calculator/product.py
@@ -1,5 +1,14 @@
-from dietary_calculator.attribute import DietaryAttribute
+from typing import Set
 
 
 class FoodProduct:
-    pass
+    """
+    A food product, which has a name and a set of dietary attributes.
+    """
+    def __init__(self, name: str, attributes: Set[str]):
+        self.name = name
+        self.attributes = attributes
+
+    def __repr__(self) -> str:
+        to_return = f"{self.__class__.__name__}({repr(self.name)}, {repr(self.attributes)})"
+        return to_return
